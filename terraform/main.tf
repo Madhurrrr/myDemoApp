2dd769b7b6ca5d385954ce16e0253e49961a62f1
Madhur <86947947+Madhurrrr@users.noreply.github.com>
2024-09-02 17:30:16 +0530

    retest

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,9 @@
 provider "azurerm" {
   features {}
   tenant_id = "00f80d0c-aac1-4ef7-b535-18de0600df3c"
+  client_id = "6a43fd50-a2f8-4970-a6a5-096de1fb3cc2"
+  client_secret = "Ess8Q~6UcWfsqQf2VmT~z3CMEAeNSBPjPG0S3bVK"
+  subscription_id = "b6ab4036-6f78-4352-8509-74b8f3e9eef2"
 }
 
 resource "azurerm_resource_group" "rgsa" {
