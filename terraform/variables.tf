0185a76da791abad06f7d08e2d8ab45800840e08
Madhur <86947947+Madhurrrr@users.noreply.github.com>
2024-09-02 17:13:54 +0530

    test push

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,11 @@
 variable "client_id" {
   description = "6a43fd50-a2f8-4970-a6a5-096de1fb3cc2"
+  type =string
+  default = "6a43fd50-a2f8-4970-a6a5-096de1fb3cc2"
 }
 
 variable "client_secret" {
   description = "Ess8Q~6UcWfsqQf2VmT~z3CMEAeNSBPjPG0S3bVK"
+  type =string
+  default = "Ess8Q~6UcWfsqQf2VmT~z3CMEAeNSBPjPG0S3bVK"
 }
