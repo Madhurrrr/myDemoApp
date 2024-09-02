b380f7d7804095b791a3ecd11dced50f2b902a53
Madhur <86947947+Madhurrrr@users.noreply.github.com>
2024-09-02 16:16:44 +0530

    test final

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "testResource"
+    storage_account_name = "testAccount"
+    container_name = "tfstate01"
+    key="teraform.tfstate"
+  }
+}
 provider "azurerm" {
   features {}
 }
